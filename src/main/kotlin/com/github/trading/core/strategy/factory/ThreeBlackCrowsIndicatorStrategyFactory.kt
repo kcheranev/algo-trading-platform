39f73632f8fc96f55143b297aef7b799bddf1b72
Konstantin Cheranev <kcheranev@gmail.com>
2025-11-07 12:35:24 +0300

    Добавлена стратегия Three Black Crows

--- src/main/kotlin/com/github/trading/core/strategy/factory/ThreeBlackCrowsIndicatorStrategyFactory.kt
+++ src/main/kotlin/com/github/trading/core/strategy/factory/ThreeBlackCrowsIndicatorStrategyFactory.kt
@@ -8,21 +8,20 @@ import com.github.trading.domain.model.TradeStrategy
 import org.springframework.stereotype.Component
 import org.ta4j.core.BarSeries
 import org.ta4j.core.BaseStrategy
-import org.ta4j.core.indicators.candles.ThreeWhiteSoldiersIndicator
+import org.ta4j.core.indicators.candles.ThreeBlackCrowsIndicator
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator
-import org.ta4j.core.num.DecimalNum
 import org.ta4j.core.rules.BooleanIndicatorRule
 
 @Component
-class ThreeWhiteSoldiersStrategyFactory : LongStrategyFactory() {
+class ThreeBlackCrowsIndicatorStrategyFactory : ShortStrategyFactory() {
 
     override fun initStrategy(parameters: StrategyParameters, series: BarSeries): TradeStrategy {
-        val barCount = parameters.getAsIntOrThrow(ThreeWhiteSoldiersStrategyLongParameter.BAR_COUNT)
-        val factor = parameters.getAsBigDecimalOrThrow(ThreeWhiteSoldiersStrategyLongParameter.FACTOR)
-        val gainPercentage = parameters.getAsBigDecimalOrThrow(ThreeWhiteSoldiersStrategyLongParameter.GAIN_PERCENTAGE)
-        val lossPercentage = parameters.getAsBigDecimalOrThrow(ThreeWhiteSoldiersStrategyLongParameter.LOSS_PERCENTAGE)
+        val barCount = parameters.getAsIntOrThrow(ThreeBlackCrowsStrategyLongParameter.BAR_COUNT)
+        val factor = parameters.getAsBigDecimalOrThrow(ThreeBlackCrowsStrategyLongParameter.FACTOR)
+        val gainPercentage = parameters.getAsBigDecimalOrThrow(ThreeBlackCrowsStrategyLongParameter.GAIN_PERCENTAGE)
+        val lossPercentage = parameters.getAsBigDecimalOrThrow(ThreeBlackCrowsStrategyLongParameter.LOSS_PERCENTAGE)
 
-        val bullishEngulfingIndicator = ThreeWhiteSoldiersIndicator(series, barCount, DecimalNum.valueOf(factor))
+        val bullishEngulfingIndicator = ThreeBlackCrowsIndicator(series, barCount, factor.toDouble())
 
         val entryRule = BooleanIndicatorRule(bullishEngulfingIndicator)
         val exitRule = StopGainRule(ClosePriceIndicator(series), gainPercentage)
@@ -30,13 +29,13 @@ class ThreeWhiteSoldiersStrategyFactory : LongStrategyFactory() {
         return buildTradeStrategy(series, BaseStrategy(entryRule, exitRule, 1))
     }
 
-    override val strategyName = "THREE_WHITE_SOLDIERS"
+    override val strategyName = "THREE_BLACK_CROWS"
 
-    override fun strategyParameterNames() = ThreeWhiteSoldiersStrategyLongParameter.entries.map(ThreeWhiteSoldiersStrategyLongParameter::alias)
+    override fun strategyParameterNames() = ThreeBlackCrowsStrategyLongParameter.entries.map(ThreeBlackCrowsStrategyLongParameter::alias)
 
 }
 
-private enum class ThreeWhiteSoldiersStrategyLongParameter(private val alias: String) : StrategyParameter {
+private enum class ThreeBlackCrowsStrategyLongParameter(private val alias: String) : StrategyParameter {
 
     BAR_COUNT("barCount"),
     FACTOR("factor"),
