e37f78a663cb843a958f93c10533b21689177483
Konstantin Cheranev <kcheranev@gmail.com>
2025-11-10 01:45:04 +0300

    Скрипт предзаполнения инструментов

---
+++ src/main/resources/db/changelog/dml/add-instruments.sql
@@ -0,0 +1,20 @@
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+	VALUES ('57bfad7d-e40c-4040-8a8a-c8b652bbb62a', 'T', 'Т-Технологии', 1, '87db07bc-0e02-4e29-90bb-05e8ef791d7b');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+	VALUES ('6f0eaba5-e1f7-4872-aae7-739d4bfc71c4', 'SBER', 'Сбер Банк', 1, 'e6123145-9665-43e0-8413-cd61b8aa9b13');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('37950d3d-d664-452e-a0d4-074a3dcfca76', 'YDEX', 'Яндекс', 1, '7de75794-a27f-4d81-a39b-492345813822');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('b2bff055-766b-4c56-8a29-42cb58e1fce8', 'LKOH', 'Лукойл', 1, '02cfdf61-6298-4c0f-a9ca-9cabc82afaf3');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('c9a52963-745c-4af9-9bc9-87ce21da2f51', 'NVTK', 'Новатэк', 1, '0da66728-6c30-44c4-9264-df8fac2467ee');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('45c9623b-8ffd-44eb-bcc5-79ad98378dde', 'GMKN', 'Норильский никель', 10, '509edd0c-129c-4ee2-934d-7f6246126da1');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('d4e7692e-1eb3-419d-876e-af7f901ad318', 'RTKM', 'Ростелеком', 10, '02eda274-10c4-4815-8e02-a8ee7eaf485b');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('9621dd32-5f5b-4fc8-aae6-ccb55052b8d3', 'GAZP', 'Газпром', 10, '962e2a95-02a9-4171-abd7-aa198dbe643a');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('2c01b17e-e5bf-4b1b-bd71-65385d4e54c1', 'PHOR', 'ФосАгро', 1, '9978b56f-782a-4a80-a4b1-a48cbecfd194');
+INSERT INTO public.instrument(id, ticker, name, lot, broker_instrument_id)
+    VALUES ('1dcf636a-5178-4971-879e-e105ce397506', 'MTSS', 'МТС', 10, 'cd8063ad-73ad-4b31-bd0d-93138d9e99a2');
